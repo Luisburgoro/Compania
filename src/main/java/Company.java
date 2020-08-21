104555cb7f045d9b58a92824b4fff05151257d23
Luisburgoro <64493029+Luisburgoro@users.noreply.github.com>
2020-08-21 15:33:46 -0400

    Update Company.java

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 // Se crea la clase Company 
 public class Company {
 
-    private String name;//Atributo perteneciente a la clase Company
+    private String name;//Atributo de acceso privado perteneciente a la clase Company
     private ArrayList<Departament>departamentArrayList;/*Es parte  de la clase company
-                                                       o el ArrayList departamentArrayList de la clase Departament es parte de la clase Company
-                                                       de manera que su vinculo es fuerte (herencia). */
+                                                       o el ArrayList departamentArrayList de tipo Departament es parte de la clase Company
+                                                       de manera que su vinculo es fuerte (composición). */
 
 }
