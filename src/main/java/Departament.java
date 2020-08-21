147339bcdd8b7e11e64036b1d7aa2564574699e7
Luisburgoro <64493029+Luisburgoro@users.noreply.github.com>
2020-08-21 15:11:16 -0400

    agregé comentarios a clase Departament

--- src/main/java/Departament.java
+++ src/main/java/Departament.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 
+// Se crea clase Departament
 public class Departament {
 
-    public Employee managedBy;
+    public Employee managedBy;//atributo perteneciente a la clase Employee
     private ArrayList<Employee>employeeArrayList;
     private ArrayList<Office>officeArrayList;
+    
+    /*- En la línea 6 de código se asocia el ArrayList de tipo Employee a la clase Departament.
+      - En la linea 7 de código se asocia el ArrayList de tipo Office a la clase Departament.
+    
 }
