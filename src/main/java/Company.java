7a10b72b21cd7092b0574a2244621d9be282f057
Luisburgoro <64493029+Luisburgoro@users.noreply.github.com>
2020-08-21 14:47:32 -0400

    se agregó comentario a clase Company

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 
+// Se crea la clase Company 
 public class Company {
 
-    private String name;
-    private ArrayList<Departament>departamentArrayList;
-
+    private String name;//Atributo perteneciente a la clase Company
+    private ArrayList<Departament>departamentArrayList;/*Es parte  de la clase company
+                                                       o el ArrayList departamentArrayList de la clase Departament es parte de la clase Company
+                                                       de manera que su vinculo es fuerte (herencia). */
 
 }
